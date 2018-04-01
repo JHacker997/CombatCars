a36c14292a892d212cf35b7ad8c6e0c3fc2b78e7
John Hacker <jhacker997@knights.ucf.edu>
2018-04-01 15:54:50 -0400

    Tried making health bar work
    Changed default max health to 1 but the debug continually says that the health is at 100.

--- Intermediate/Build/Win64/UE4Editor/Inc/CombatCars2/CombatCars2WheelFront.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/CombatCars2/CombatCars2WheelFront.generated.h
@@ -13,9 +13,9 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define COMBATCARS2_CombatCars2WheelFront_generated_h
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS_NO_PURE_DECLS
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS_NO_PURE_DECLS \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUCombatCars2WheelFront(); \
 	friend COMBATCARS2_API class UClass* Z_Construct_UClass_UCombatCars2WheelFront(); \
@@ -25,7 +25,7 @@ public: \
 	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
 
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS \
 private: \
 	static void StaticRegisterNativesUCombatCars2WheelFront(); \
 	friend COMBATCARS2_API class UClass* Z_Construct_UClass_UCombatCars2WheelFront(); \
@@ -35,7 +35,7 @@ public: \
 	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
 
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_STANDARD_CONSTRUCTORS \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UCombatCars2WheelFront(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCombatCars2WheelFront) \
@@ -48,7 +48,7 @@ private: \
 public:
 
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_ENHANCED_CONSTRUCTORS \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API UCombatCars2WheelFront(UCombatCars2WheelFront&&); \
@@ -59,32 +59,32 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCombatCars2WheelFront); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UCombatCars2WheelFront)
 
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_9_PROLOG
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_GENERATED_BODY_LEGACY \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_9_PROLOG
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_STANDARD_CONSTRUCTORS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_GENERATED_BODY \
+#define CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS_NO_PURE_DECLS \
-	CC3_Source_CombatCars2_CombatCars2WheelFront_h_12_ENHANCED_CONSTRUCTORS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_PRIVATE_PROPERTY_OFFSET \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_INCLASS_NO_PURE_DECLS \
+	CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID CC3_Source_CombatCars2_CombatCars2WheelFront_h
+#define CURRENT_FILE_ID CombatCars3_Source_CombatCars2_CombatCars2WheelFront_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
